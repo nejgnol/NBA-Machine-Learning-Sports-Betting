37a41551ae9f1c8b3d842a6db61f598c09c8569e
nejgnol <125590586+nejgnol@users.noreply.github.com>
2023-02-17 14:25:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -66,6 +66,8 @@ def main():
             print(Fore.RED, "--------------Games list not up to date for todays games!!! Scraping disabled until list is updated.--------------")
             print(Style.RESET_ALL)
             odds = None
+        elif not games:
+            print("no NBA game scheduled for today.")
         else:
             print(f"------------------{args.odds} odds data------------------")
             for g in odds.keys():
@@ -74,6 +76,9 @@ def main():
     else:
         data = get_todays_games_json(todays_games_url)
         games = create_todays_games(data)
+        if not games:
+            print("no NBA game scheduled for today.")
+            return
     data = get_json_data(data_url)
     df = to_data_frame(data)
     data, todays_games_uo, frame_ml, home_team_odds, away_team_odds = createTodaysGames(games, df, odds)
@@ -84,8 +89,7 @@ def main():
         print("-------------------------------------------------------")
     if args.xgb:
         print("---------------XGBoost Model Predictions---------------")
-        XGBoost_Runner.xgb_runner(data, todays_games_uo, frame_ml, games, home_team_odds, away_team_odds)
-        print("-------------------------------------------------------")
+        XGBoost_Runner.xgb_runner(data, todays_games_uo, frame_ml,
     if args.A:
         print("---------------XGBoost Model Predictions---------------")
         XGBoost_Runner.xgb_runner(data, todays_games_uo, frame_ml, games, home_team_odds, away_team_odds)
