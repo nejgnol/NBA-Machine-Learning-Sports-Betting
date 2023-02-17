f72344b882a8804a298661ba08f3fd782b79101d
nejgnol <125590586+nejgnol@users.noreply.github.com>
2023-02-17 14:42:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from src.Utils.Dictionaries import team_index_current
 from src.Utils.tools import create_todays_games_from_odds, get_json_data, to_data_frame, get_todays_games_json, create_todays_games
 from src.DataProviders.SbrOddsProvider import SbrOddsProvider
 
+tf.saved_model.save(model, export_dir)
 
 todays_games_url = 'https://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2022/scores/00_todays_scores.json'
 data_url = 'https://stats.nba.com/stats/leaguedashteamstats?' \
